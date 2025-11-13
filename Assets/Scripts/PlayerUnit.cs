e022d20d6cde349a08464cd9e1f379d120b15694
rattikan-mata <rattikan.mata@bumail.net>
2025-11-13 23:42:31 +0700

    PlayerUnit inheritance

--- Assets/Scripts/PlayerUnit.cs
+++ Assets/Scripts/PlayerUnit.cs
@@ -1,3 +1,35 @@
-﻿public class PlayerUnit
+﻿using UnityEngine;
+
+public class PlayerUnit : AbstractUnit
 {
+    public void InteractWith(IElementInteractive targetObject)
+    {
+        bool success = targetObject.HandleElementContact(_stats.CurrentElement, this);
+
+        if (!success)
+        {
+            TakeDamage(1);
+            Debug.Log("Interaction Failed! Took 1 Damage.");
+        }
+        else
+        {
+            Debug.Log("Interaction Success!");
+        }
+    }
+
+    public void ShiftElement()
+    {
+        ElementType newElement = _stats.CurrentElement == ElementType.Present ? ElementType.Past : ElementType.Present;
+        _stats.ChangeElement(newElement);
+    }
+
+    public override void HandleElementContact(ElementType element, IElementInteractive sourceObject)
+    {
+        Debug.Log($"Player received element signal: {element}");
+    }
+
+    protected override void Die()
+    {
+        Debug.Log("Player Died. Game Over.");
+    }
 }
